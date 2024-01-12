092b751c5c0e0fd298243517ef82ad128b77ea49
mf0x <mikefox9000@gmail.com>
2024-01-12 15:56:26 -0500

    comment out redirect url finder, find and print single-column duplicates, clean up duplicate section

--- wiki_trawler.py
+++ wiki_trawler.py
@@ -16,6 +16,7 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # Open the CSV file for writing
 csv_file = open('wiki_rappers.csv', 'w', newline='', encoding='utf-8')
 csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["Link Title", "Link URL"])
 
 # Find all hyperlinks in the HTML source
 list_divs = soup.find_all('div', class_='div-col')
@@ -36,18 +37,18 @@ for div in list_divs:
 
             # Process the article URL and extract relevant information
             # this doesn't seem to actually work; it just returns the same url, not the redirect
-            line_response = requests.get(article_url)
-            true_url = line_response.url
+            # line_response = requests.get(article_url)
+            # true_url = line_response.url
 
-            if true_url != article_url:
-                print(f"found redirect for {title}: {article_url} -> {true_url}")
+            # if true_url != article_url:
+            #     print(f"found redirect for {title}: {article_url} -> {true_url}")
             
             # Write the extracted information to the CSV file
-            csv_writer.writerow([title, true_url])
+            csv_writer.writerow([title, article_url])
 
-            print(f"Writing line {count}")
+            # print(f"Writing line {count}")
             
-print(count)
+print(f"Finished writing {count} entries")
 
 # Close the CSV file
 csv_file.close()
@@ -56,20 +57,22 @@ csv_file.close()
 # 1. Read CSV
 df = pd.read_csv("wiki_rappers.csv")
 
-print(df.duplicated().sum())
+print(f"{df.duplicated().sum()} duplicates found:")
+print(df[df.duplicated()])
 
 # 2(a). For complete row duplicate
 df.drop_duplicates(inplace=True)
-             
+print(f"Removing duplicates\n{df.duplicated().sum()} duplicates remaining")
+            
 # 2(b). For partials
-# pd.drop_duplicates(subset=['Date', 'Time', <other_fields>], inplace=True)
-
-print(df.duplicated().sum())
+title_dups = df[df.duplicated(subset=["Link Title"], keep=False)]
+url_dups = df[df.duplicated(subset=["Link URL"], keep=False)]
+print(f"Link Title duplicates:\n{title_dups}\nLink URL duplicates:\n{url_dups}")
 
 # 3. Save then
 df.to_csv("wiki_rappers.csv", index=False)
 
 deduped_file = open('wiki_rappers.csv', 'r', encoding='utf-8')
 row_count = sum(1 for row in deduped_file)
-print(row_count)
+print(f"{row_count} rows in final file")
 deduped_file.close()
